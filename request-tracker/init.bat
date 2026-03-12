8153d3ee3a60b70700b1e72858ece18aa90e37a5
Isbel Ochoa <isbel8ai@gmail.com>
2026-03-12 19:44:31 +0200

    Improve initialization script form on Request Tracker configuration

--- request-tracker/init.bat
+++ request-tracker/init.bat
@@ -1,3 +1,5 @@
+@echo off
 podman compose up -d postgres
+timeout /t 5
 podman compose run --rm rt bash -c "cd /opt/rt && perl ./sbin/rt-setup-database --dba rtuser --dba-password rtpass --action init --skip-create"
 podman compose up -d
